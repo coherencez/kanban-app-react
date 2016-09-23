31effebbfce73a1d1cd86b95210f196fbe3066ef
Thomas Swann <tkswann@mac.com>
2016-09-22 20:31:44 -0500

    added delete function to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -26,7 +26,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes}/>
+        <Notes notes={notes} onDelete={this.deleteNote}/>
       </div>
     )
   }
@@ -48,5 +48,12 @@ export default class App extends React.Component {
           task: 'New Task'
         }
     ]})
+  }
+  deleteNote = (id, e) => {
+    e.stopPropagation()
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    })
   }
 }
