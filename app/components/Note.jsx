c2a42a6219def83de69f5975e2c4c8f4b5e4bb4c
Thomas Swann <tkswann@mac.com>
2016-09-22 21:04:32 -0500

    refactored so rendering of Note of now controlled by Notes

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
-export default ({task, onDelete}) => {
-  return (
-    <div>
-      <span>{task}</span>
-      <button onClick={onDelete}>x</button>
-    </div>
-  )
-}
+export default ({children, ...props}) => (
+  <div {...props}>
+    {children}
+  </div>
+)
