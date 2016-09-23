7beb30d3b88e1eaa3b22aa7c39613337791f2399
Thomas Swann <tkswann@mac.com>
2016-09-22 19:40:53 -0500

    data now being passed in as prop from parent App

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import uuid from 'uuid'
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
+// const notes = [
+//   {
+//     id: uuid.v4(),
+//     task: 'Learn React'
+//   },
+//   {
+//     id: uuid.v4(),
+//     task: 'Do laundry'
+//   }
+// ];
 
-export default () => (
+export default ({notes}) => (
   // mix JS with JSX, return list of items for React to render
   <ul>{notes.map(note =>
     // key prop used to oder the list
