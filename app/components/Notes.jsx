a4961b1cdb3308037b608f68418c6a0766de3917
Thomas Swann <tkswann@mac.com>
2016-09-22 20:27:18 -0500

    setup communication from Notes to App for delete, dummy function to prevent app crash if no delete func provided

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Note from './Note'
 
 
-export default ({notes, ...props}) => (
-  // mix JS with JSX, return list of items for React to render
-  <ul>{notes.map(note =>
-    // key prop used to oder the list
-    <li key={note.id}><Note task={note.task} /></li>
+export default ({notes, onDelete=() => {}}) => (
+  <ul>{notes.map(({id, task}) =>
+    <li key={id}>
+      <Note
+        onDelete={onDelete.bind(null,id)}
+        task={task}  />
+    </li>
   )}</ul>
 )
